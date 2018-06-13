388a0f6d947fa6633f26037a75994012b8034655
Watt Iamsuri <wiamsuri@gmail.com>
2018-06-13 12:01:38 +0700

    add for loop problems

--- 1-conditionals-and-loops/1-while-loop.py
+++ 1-conditionals-and-loops/1-while-loop.py
@@ -5,3 +5,14 @@ while #condition here
 
 list_of_number = [40, 32, 73, 8, 16]
 # Write a while loop to print each number in the list
+
+
+# Print mutiplication table to 12 using while loop
+number = 5
+
+
+# Print out result of factorial
+result = 0
+while #condition here
+
+print(result)
---
+++ 1-conditionals-and-loops/2-for-loop.py
@@ -0,0 +1,13 @@
+# Print out number 0 to 10
+for
+
+
+# Print out the words
+list_of_words = ['Hello', 'World', 'Foo', 'Bar']
+
+
+# Print out the square of each number from 0 to 10
+
+
+# Print out Fibonacci sequence up to the tenth number using
+fibonacci = [0, 1]
