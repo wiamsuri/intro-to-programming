dd006b30d6df31ec2212dd81a7d6eb784d3f14ad
Watt Iamsuri <wiamsuri@gmail.com>
2018-07-24 22:20:36 +0700

    loops practice problem

---
+++ 1-conditionals-and-loops/3-practice.py
@@ -0,0 +1,25 @@
+# 3-Practice
+
+def factorial(num):
+    # TODO: Find factorial of num input
+    # Code goes here
+    return num
+
+print(factorial(4))
+print(factorial(8))
+
+
+input_word_list = ['lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit']
+def find_longest_word(word_list):
+    # TODO: Find the longest word in the list
+    # Code goes here
+    return word_list[0]
+
+print(find_longest_word(input_word_list))
+
+
+def e():
+    # TODO: Find the value of e (Euler's number)
+    # hint: Sum of 1/n! from 0 to infinity
+    # Code goes here
+    return 2.7
